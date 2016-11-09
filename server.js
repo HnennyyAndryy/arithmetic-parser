01b5c4ecc591641de58f426801a297abe5d63bca
Cat <hnennyy_andryy@hotmail.com>
2016-11-09 23:15:31 +0200

    another useless fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var server_port = process.env.PORT || 8080;
-var server_ip_address = process.env.IP || 'localhost';
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var tokenizer = require('./tokenizer.js');
 var treebuilder = require('./treebuilder.js');
